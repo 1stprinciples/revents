5ad44586d20bf936874dccda27c3bd9bfb770767
1stprinciples <sughanthans1@gmail.com>
2022-02-06 15:06:30 +0100

    folder structure change

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+
+  return ( // Code written within this curve braces is written in JSX (Note: it is not HTML)
+    /* Creating a div element with a class of "App" */
+    <div className = "App">
+      <h1>Re-vents</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; // ReactDOM is used in case of web apps - React Document Object Model
+import './app/layout/styles.css'
+import App from './app/layout/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root'); //Creating a var for the root element
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function render(){ // A function that replaces the root element using App 
+  ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot){ // if there is a hot module available then inject the changes
+  module.hot.accept('./app/layout/App.jsx',function(){
+    setTimeout(render);
+  })
+}
+
+render();
